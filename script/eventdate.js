55efa5ce241050a9e0a912a5a9a1b7cca9734dec
vv01f <vv01f@users.noreply.github.com>
2023-06-05 15:10:03 +0200

    fix timezone

--- script/eventdate.js
+++ script/eventdate.js
@@ -3,14 +3,45 @@
 //~ import * as RRule from 'https://jakubroztocil.github.io/rrule/dist/es5/rrule.js';
 import * as RRule from 'https://bitcoin-dresden.de/script/rrule.min.js';
 
+var now = new Date();
+function getCETorCESTDateOffset() {
+	var utcOffset = now.getTimezoneOffset();
+	var cetOffset = utcOffset + 60;
+	var cestOffset = utcOffset + 120;
+	var cetOffsetInMilliseconds = cetOffset * 60 * 1000;
+	var cestOffsetInMilliseconds = cestOffset * 60 * 1000;
+
+	var cestDateStart = new Date();
+	var cestDateFinish = new Date();
+	var localDateTime = now.getTime();
+	var cestDateStartTime;
+	var cestDateFinishTime;
+	var result;
+
+	cestDateStart.setTime(Date.parse('29 March ' + now.getFullYear() + ' 02:00:00 GMT+0100'));
+	cestDateFinish.setTime(Date.parse('25 October ' + now.getFullYear() + ' 03:00:00 GMT+0200'));
+
+	cestDateStartTime = cestDateStart.getTime();
+	cestDateFinishTime = cestDateFinish.getTime();
+
+	if(localDateTime >= cestDateStartTime && localDateTime <= cestDateFinishTime) {
+		result = cestOffsetInMilliseconds;
+	} else {
+		result = cetOffsetInMilliseconds;
+	}
+
+	return result;
+}
+
+var localTimeOffset=getCETorCESTDateOffset();
+
 document.addEventListener("DOMContentLoaded", function(event) { 
 
 	for(const tag of document.getElementsByTagName("time") ){
 		if (tag.getAttribute('data-rrule') !== null) {
-			var now = new Date();
 			var rule = rrule.RRule.fromString(tag.getAttribute('data-rrule').toUpperCase());
 			var e = rule.after(now);
-			tag.setAttribute('datetime', new Date(e - -now.getTimezoneOffset()*60000).toISOString());
+			tag.setAttribute('datetime', new Date.UTC(e - -localTimeOffset).toISOString());
 			var f = new Intl.DateTimeFormat("de-DE", { //timeZoneName: "short", second: "numeric" | day: "2-digit" 
 				 timeZone: "UTC", year: "numeric", month: "long", day: "numeric", weekday: "long", hour: "numeric", minute: "numeric" }).format(e).replace(' um ',' ab ');
 			//~ console.log(f);
